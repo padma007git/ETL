e3122cac0c73d3c10b3170e334d233dcba6d7578
padmasai <padmasaiarava123@gmail.com>
2026-06-30 18:10:37 +0530

    solvce identatation error

--- etl_dag.py
+++ etl_dag.py
@@ -260,35 +260,37 @@ with DAG(
     with TaskGroup(group_id="silver_layer") as silver_group:
 	
         create_customer_silver = BigQueryInsertJobOperator(
-			task_id="create_customer_silver",
-			configuration={
-				"query": {
-					"query": f"""
-					CREATE OR REPLACE TABLE `{PROJECT_ID}.silver.customer` AS
-					SELECT
-						TRIM(CID) AS customer_id,
-						BDATE AS birth_date,
-						CASE
-							WHEN UPPER(TRIM(GEN))='M' THEN 'Male'
-							WHEN UPPER(TRIM(GEN))='F' THEN 'Female'
-							ELSE 'Unknown'
-						END AS gender,
-						CURRENT_TIMESTAMP() AS load_timestamp
-						FROM (
-					SELECT *,ROW_NUMBER() OVER(PARTITION BY CID ORDER BY BDATE DESC) rn
-						FROM `{PROJECT_ID}.bronze.customer_raw`
-						)WHERE rn=1 AND CID IS NOT NULL;
-					""",
-					"useLegacySql": False,
-				}
-			},
-			location="US",
+	task_id="create_customer_silver",
+	configuration={
+	"query": {
+		"query": f"""
+		CREATE OR REPLACE TABLE `{PROJECT_ID}.silver.customer` AS
+		SELECT
+		TRIM(CID) AS customer_id,
+		BDATE AS birth_date,
+		CASE
+			WHEN UPPER(TRIM(GEN))='M' THEN 'Male'
+			WHEN UPPER(TRIM(GEN))='F' THEN 'Female'
+			ELSE 'Unknown'
+		END AS gender,
+		CURRENT_TIMESTAMP() AS load_timestamp
+		FROM 
+                (
+		SELECT *,ROW_NUMBER() OVER(PARTITION BY CID ORDER BY BDATE DESC) rn
+		FROM `{PROJECT_ID}.bronze.customer_raw`
 		)
-		audit_customer_silver = BigQueryInsertJobOperator(
-		task_id="audit_customer_silver",
-		configuration={
-			"query": {
-				"query": f"""
+                WHERE rn=1 AND CID IS NOT NULL;
+		""",
+		"useLegacySql": False,
+		}
+		},
+		location="US",
+	)
+	audit_customer_silver = BigQueryInsertJobOperator(
+	task_id="audit_customer_silver",
+	configuration={
+		"query": {
+			"query": f"""
 		MERGE `{PROJECT_ID}.audit.pipeline_audit` T
 		USING (
 
@@ -335,38 +337,38 @@ with DAG(
 			}
 		},
 		location="US",
-		)
+	)
 		
-		create_location_silver = BigQueryInsertJobOperator(
-			task_id="create_location_silver",
-			configuration={
-				"query": {
-					"query": f"""
-					CREATE OR REPLACE TABLE `{PROJECT_ID}.silver.location` AS
-					SELECT
-						TRIM(string_field_0) AS customer_id,
-						TRIM(string_field_1) AS country,
-						CURRENT_TIMESTAMP() AS load_timestamp
-					FROM
-						(
-					SELECT *,
-						ROW_NUMBER() OVER(
-						PARTITION BY string_field_0 ORDER BY string_field_0) rn
-					FROM `{PROJECT_ID}.bronze.location_raw`)
-						WHERE rn=1 AND string_field_0 IS NOT NULL;
-					""",
-					"useLegacySql": False,
-				}
-			},
+	create_location_silver = BigQueryInsertJobOperator(
+	task_id="create_location_silver",
+	configuration={
+		"query": {
+			"query": f"""
+		CREATE OR REPLACE TABLE `{PROJECT_ID}.silver.location` AS
+		SELECT
+			TRIM(string_field_0) AS customer_id,
+			TRIM(string_field_1) AS country,
+			CURRENT_TIMESTAMP() AS load_timestamp
+			FROM
+			(
+			SELECT *,
+			ROW_NUMBER() OVER(
+			PARTITION BY string_field_0 ORDER BY string_field_0) rn
+			FROM `{PROJECT_ID}.bronze.location_raw`)
+			WHERE rn=1 AND string_field_0 IS NOT NULL;
+			""",
+			"useLegacySql": False,
+			}
+		},
 			location="US",
-		)
+	)
 
 
-		audit_location_silver = BigQueryInsertJobOperator(
-		task_id="audit_location_silver",
-		configuration={
-			"query": {
-				"query": f"""
+	audit_location_silver = BigQueryInsertJobOperator(
+	task_id="audit_location_silver",
+	configuration={
+	"query": {
+		"query": f"""
 
 		MERGE `{PROJECT_ID}.audit.pipeline_audit` T
 			USING (
@@ -411,47 +413,47 @@ with DAG(
 			}
 		},
 		location="US",
-		)
+	)
 		
-		create_product_silver = BigQueryInsertJobOperator(
-			task_id="create_product_silver",
-			configuration={
-				"query": {
-					"query": f"""
-					 CREATE OR REPLACE TABLE `{PROJECT_ID}.silver.product` AS
-				 SELECT
-					TRIM(ID) AS product_id,
-					INITCAP(TRIM(CAT)) AS category,
-					INITCAP(TRIM(SUBCAT)) AS sub_category,
-					CASE
-						WHEN MAINTENANCE = TRUE THEN 'YES'
-						WHEN MAINTENANCE = FALSE THEN 'NO'
-						ELSE 'UNKNOWN'
-					END AS maintenance,
-					CURRENT_TIMESTAMP() AS load_timestamp FROM
-				(
-				SELECT *,
-					ROW_NUMBER() OVER
-				(
-					PARTITION BY ID
-					ORDER BY ID
-				) rn
-					FROM `{PROJECT_ID}.bronze.product_raw`
-				)
-					WHERE rn = 1 AND ID IS NOT NULL;
-				""",
-					"useLegacySql": False,
-				}
-			},
-			location="US",
-		)
+	create_product_silver = BigQueryInsertJobOperator(
+	task_id="create_product_silver",
+	configuration={
+	"query": {
+		"query": f"""
+	CREATE OR REPLACE TABLE `{PROJECT_ID}.silver.product` AS
+	SELECT
+		TRIM(ID) AS product_id,
+		INITCAP(TRIM(CAT)) AS category,
+		INITCAP(TRIM(SUBCAT)) AS sub_category,
+		CASE
+		    WHEN MAINTENANCE = TRUE THEN 'YES'
+		    WHEN MAINTENANCE = FALSE THEN 'NO'
+		    ELSE 'UNKNOWN'
+		END AS maintenance,
+		CURRENT_TIMESTAMP() AS load_timestamp FROM
+		(
+		SELECT *,
+			ROW_NUMBER() OVER
+			(
+			PARTITION BY ID
+			ORDER BY ID
+			) rn
+			FROM `{PROJECT_ID}.bronze.product_raw`
+			)
+			WHERE rn = 1 AND ID IS NOT NULL;
+		""",
+		"useLegacySql": False,
+		}
+	},
+		location="US",
+	)
 
 
-		audit_product_silver = BigQueryInsertJobOperator(
-		task_id="audit_product_silver",
-		configuration={
-			"query": {
-				"query": f"""
+	audit_product_silver = BigQueryInsertJobOperator(
+	task_id="audit_product_silver",
+	configuration={
+	"query": {
+		"query": f"""
 		MERGE `{PROJECT_ID}.audit.pipeline_audit` T
 		USING (
 		SELECT
@@ -517,11 +519,11 @@ with DAG(
         },
         )
 	
-		audit_customer_gold = BigQueryInsertJobOperator(
-		task_id="audit_customer_gold",
-		configuration={
-			"query": {
-				"query": f"""
+	audit_customer_gold = BigQueryInsertJobOperator(
+	task_id="audit_customer_gold",
+	configuration={
+	"query": {
+		"query": f"""
 		MERGE `{PROJECT_ID}.audit.pipeline_audit` T
 		USING (
 		SELECT
@@ -568,42 +570,42 @@ with DAG(
 		location="US",
 		)
 
-		gold_location = BigQueryInsertJobOperator(
-		task_id="gold_location",
-		configuration={
-			"query": {
-				"query": """
-				CREATE OR REPLACE TABLE `etl-demo-project-500808.gold.location` AS
-				SELECT *
-				FROM `etl-demo-project-500808.silver.location`;
-				""",
-				"useLegacySql": False,
-			}
+	gold_location = BigQueryInsertJobOperator(
+	task_id="gold_location",
+	configuration={
+	"query": {
+		"query": """
+	CREATE OR REPLACE TABLE `etl-demo-project-500808.gold.location` AS
+	SELECT *
+		FROM `etl-demo-project-500808.silver.location`;
+		""",
+		"useLegacySql": False,
+		}
 		},
-		)
+	)
 
-		audit_location_gold = BigQueryInsertJobOperator(
-		task_id="audit_location_gold",
-		configuration={
-			"query": {
-				"query": f"""
-		MERGE `{PROJECT_ID}.audit.pipeline_audit` T
-		USING (
-		SELECT
-			'silver_etl_pipeline_002' AS pipeline_name,
-			'GOLD' AS layer,
-			'LOC_A101.csv' AS source_file,
-			'location' AS target_table,
-			COUNT(*) AS records_loaded,
-			'SUCCESS' AS status,
-			CURRENT_TIMESTAMP() AS start_time,
-			CURRENT_TIMESTAMP() AS end_time
+	audit_location_gold = BigQueryInsertJobOperator(
+	task_id="audit_location_gold",
+	configuration={
+	"query": {
+		"query": f"""
+	MERGE `{PROJECT_ID}.audit.pipeline_audit` T
+	USING (
+	SELECT
+		'silver_etl_pipeline_002' AS pipeline_name,
+		'GOLD' AS layer,
+		'LOC_A101.csv' AS source_file,
+		'location' AS target_table,
+		COUNT(*) AS records_loaded,
+		'SUCCESS' AS status,
+		CURRENT_TIMESTAMP() AS start_time,
+		CURRENT_TIMESTAMP() AS end_time
 		FROM `{PROJECT_ID}.gold.location`
 		) S
 		ON
-			T.source_file = S.source_file
-			AND T.layer = S.layer
-			AND T.target_table = S.target_table
+		T.source_file = S.source_file
+		AND T.layer = S.layer
+		AND T.target_table = S.target_table
 		WHEN NOT MATCHED THEN
 		INSERT (
 			pipeline_name,
@@ -631,27 +633,27 @@ with DAG(
 			}
 		},
 		location="US",
-		)
+	)
 
-		gold_product = BigQueryInsertJobOperator(
-		task_id="gold_product",
-		configuration={
-			"query": {
-				"query": """
-				CREATE OR REPLACE TABLE `etl-demo-project-500808.gold.product` AS
-				SELECT *
-				FROM `etl-demo-project-500808.silver.product`;
-				""",
-				"useLegacySql": False,
-			}
+	gold_product = BigQueryInsertJobOperator(
+	task_id="gold_product",
+	configuration={
+	"query": {
+		"query": """
+		CREATE OR REPLACE TABLE `etl-demo-project-500808.gold.product` AS
+		SELECT *
+		FROM `etl-demo-project-500808.silver.product`;
+		""",
+		"useLegacySql": False,
+		}
 		},
-		)
+	)
 		 
-		audit_product_gold = BigQueryInsertJobOperator(
-		task_id="audit_product_gold",
-		configuration={
-			"query": {
-				"query": f"""
+	audit_product_gold = BigQueryInsertJobOperator(
+	task_id="audit_product_gold",
+	configuration={
+	query": {
+		"query": f"""
 		MERGE `{PROJECT_ID}.audit.pipeline_audit` T
 		USING (
 		SELECT
@@ -692,11 +694,11 @@ with DAG(
 			S.end_time
 		)
 		""",
-				"useLegacySql": False,
+			"useLegacySql": False,
 			}
 		},
 		location="US",
-		)
+	)
         gold_customer >> audit_customer_gold >> \
         gold_location >> audit_location_gold >> \
         gold_product >> audit_product_gold
